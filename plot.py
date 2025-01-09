698c458bc58f6268561a51eae97d7c559c00ccd4
[Pranav Joshi] <[pranav.joshi@iitgn.ac.in]>
2025-01-09 10:00:09 +0530

    fixing errors

--- plot.py
+++ plot.py
@@ -1,9 +1,11 @@
+""" Simple example of plotting using pyplot """
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.linspace(-2*np.pi,2*np.pi,1000)
-y = np.sin(x)
-plt.plot(x,y)
-plt.xlim(-2*np.pi,2*np.pi)
-plt.ylim(-1.5,1.5)
-plt.legend(["$ y=sin(x) $"])
-plt.show()
+if __name__ == "__main__":
+    x = np.linspace(-2*np.pi,2*np.pi,1000)
+    y = np.sin(x)
+    plt.plot(x,y)
+    plt.xlim(-2*np.pi,2*np.pi)
+    plt.ylim(-1.5,1.5)
+    plt.legend(["$ y=sin(x) $"])
+    plt.show()
